2a1e09f76f014ee1d9eed942307568e11c73e7f0
Tanmoy <majitanmoy14@gmail.com>
2024-09-06 17:53:00 +0530

    add

---
+++ practics.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Tree{
+    public:
+     int data;
+     Tree* left;
+     Tree* right;
+     Tree(int data){
+
+     }
+};
+int main(){
+    
+    return 0;
+}
